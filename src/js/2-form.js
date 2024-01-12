7da0c7a5dfafd65a56abecfb72e217a118419760
BaseOfExistence <139703947+BaseOfExistence@users.noreply.github.com>
2024-01-12 16:08:07 +0200

    wdwd

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,14 +3,17 @@ const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 if (localStorage.getItem("feedback-form-state")) {
     const feedbackSave = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (feedbackSave.email && feedbackSave.email !== "") {
+    if (feedbackSave.email && feedbackSave.email.trim() !== "") {
         email.value = feedbackSave.email;
     }
-    if (feedbackSave.message && feedbackSave.message !== "") {
+    if (feedbackSave.message && feedbackSave.message.trim() !== "") {
         message.value = feedbackSave.message;
     }
 }
-const feedback = {}
+const feedback = {
+    email: "",
+    message: ""
+}
 form.addEventListener("input", (event) => {
     if (event.target.tagName.toLowerCase() === "input") {
         feedback.email = event.target.value.trim();
@@ -22,7 +25,7 @@ form.addEventListener("input", (event) => {
 })
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (email.value && message.value) {
+    if (email.value && email.value !== "" && message.value && message.value !== "") {
     console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
     email.value = "";
     message.value = "";
