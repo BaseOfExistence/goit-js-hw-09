2f9d4c73b454168badbc597591cdc2fcb0ba809c
BaseOfExistence <139703947+BaseOfExistence@users.noreply.github.com>
2024-01-14 09:46:22 +0200

    DAAAAAAAAAAA

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,34 +1,32 @@
 const form = document.querySelector(".feedback-form");
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
+let feedbackSave = {};
+
 if (localStorage.getItem("feedback-form-state")) {
-    const feedbackSave = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (feedbackSave.email && feedbackSave.email !== "") {
-        email.value = feedbackSave.email;
-    }
-    if (feedbackSave.message && feedbackSave.message !== "") {
-        message.value = feedbackSave.message;
-    }
+  feedbackSave = JSON.parse(localStorage.getItem("feedback-form-state"));
+  if (feedbackSave.email && feedbackSave.email !== "") {
+    email.value = feedbackSave.email;
+  }
+  if (feedbackSave.message && feedbackSave.message !== "") {
+    message.value = feedbackSave.message;
+  }
 }
+
 form.addEventListener("input", (event) => {
-    const feedback = {
-        email: "",
-        message: ""
-    }
-    if (event.target.tagName.toLowerCase() === "input") {
-        feedback.email = event.target.value.trim();
-    }
-    if (event.target.tagName.toLowerCase() === "textarea") {
-        feedback.message = event.target.value.trim();
-    }
-    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
-})
+  const feedback = {
+    email: email.value.trim(),
+    message: message.value.trim()
+  };
+  localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+});
+
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (email.value.trim() && email.value !== "" && message.value.trim() && message.value !== "") {
-        console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-        email.value = "";
-        message.value = "";
-        localStorage.removeItem("feedback-form-state");
-    }
+  event.preventDefault();
+  if (email.value.trim() && email.value !== "" && message.value.trim() && message.value !== "") {
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    email.value = "";
+    message.value = "";
+    localStorage.removeItem("feedback-form-state");
+  }
 });
